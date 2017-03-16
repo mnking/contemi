19b3937e4a9314661962f3a24e023c09a6bed1ca
vuong.nguyen <vuong.nguyen@contemi.com.vn>
2017-03-16 15:11:28 +0700

    update

---
+++ front-end/Form.php
@@ -0,0 +1,16 @@
+<div class="content-control">
+    <input id="partner-email" class="form-control" type="text" data-ng-model="partner.Email"
+           name="partnerEmail"
+           placeholder="<?php echo JText::_("MOD_DASHBOARD_MANAGEMENT_PARTNER_EMAIL_PLACEHOLDER") ?>"
+           data-ng-required="true"
+           data-ng-pattern="/^[_a-z0-9A-Z]+(\.[_a-z0-9A-Z]+)*@[a-z0-9A-Z-]+(\.[a-z0-9A-Z-]+)*(\.[a-zA-Z]{2,4})$/"
+    />
+    <div data-ng-if="formSavePartner.$submitted">
+                            <span data-ng-show="formSavePartner.partnerEmail.$error.required"
+                                  data-tooltip="<?php echo JText::_("MOD_DASHBOARD_COMMON_BLANK_ERROR_MESSAGE")?>"
+                                  data-tooltip-placement="top-left" class="fa fa-exclamation-triangle form-control-feedback"></span>
+        <span data-ng-show="formSavePartner.partnerEmail.$error.pattern"
+              data-tooltip="<?php echo JText::_("ERROR_EMAIL_INVALID")?>"
+              data-tooltip-placement="top-left" class="fa fa-exclamation-triangle form-control-feedback"></span>
+    </div>
+</div>
