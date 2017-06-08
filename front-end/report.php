9a68318e9b6ceed7753f8d213e1162b5b875aec1
vuong.nguyen <vuong.nguyen@contemi.com.vn>
2017-06-08 14:54:21 +0700

    update

---
+++ front-end/report.php
@@ -0,0 +1,41 @@
+<div class="report-module">
+    <div class="row">
+        <div class="col-lg-3 col-sm-6"><!-- START widget-->
+            <div class="panel widget bg-primary">
+                <div class="row row-table">
+                    <div class="col-xs-4 text-center bg-primary-dark pv-lg">
+                        <em class="fa fa-calendar fa-3x"></em>
+                    </div>
+                    <div class="col-xs-8 pv-lg">
+                        <div class="h2 mt0">1700</div>
+                        <div class="text-uppercase">Uploads</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-3 col-sm-6"><!-- START widget-->
+            <div class="panel widget bg-purple">
+                <div class="row row-table">
+                    <div class="col-xs-4 text-center bg-purple-dark pv-lg"><em class="icon-globe fa-3x"></em></div>
+                    <div class="col-xs-8 pv-lg">
+                        <div class="h2 mt0">700
+                            <small>GB</small>
+                        </div>
+                        <div class="text-uppercase">Quota</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-3 col-md-6 col-sm-12"><!-- START widget-->
+            <div class="panel widget bg-green">
+                <div class="row row-table">
+                    <div class="col-xs-4 text-center bg-green-dark pv-lg"><em class="icon-bubbles fa-3x"></em></div>
+                    <div class="col-xs-8 pv-lg">
+                        <div class="h2 mt0">500</div>
+                        <div class="text-uppercase">Reviews</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
